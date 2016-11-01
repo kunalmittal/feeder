263ec51c09a88f51e600a837be09aed510bc0c42
KUNAL MITTAL <kunal@cse.iitb.ac.in>
2016-11-01 20:08:55 +0530

    committing in required format

---
+++ lab10/adminApp/urls.py
@@ -0,0 +1,27 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.loginForm, name="loginForm"),
+    url(r'^welcome/$', views.welcome, name="welcome"),
+    url(r'^addCourse/$', views.addCourse, name="addCourse"),
+
+    url(r'^welcome/(?P<course_number>[\d\D]+)/', views.coursePage, name="coursePage"),
+    url(r'^welcome/enroll', views.enroll, name="coursePage"),
+
+    url(r'^logout_admin/$', views.logout_admin, name="logout_admin"),
+
+    url(r'^deadlineform/(?P<course_number>[\d\D]+)/',views.deadlineform,name="deadline"),
+    url(r'^mid_deadline/$',views.mid_deadline,name="mid_deadline"),
+    url(r'^end_deadline/$',views.end_deadline,name="end_deadline"),
+
+    url(r'^feedbackform/(?P<course_number>[\d\D]+)/',views.feedbackform,name="feedback"),
+
+    url(r'^mid_feedback/$',views.mid_feedback,name="mid_feedback"),
+    url(r'^end_feedback/$',views.end_feedback,name="end_feedback"),
+
+
+
+
+
+]
---
+++ lab10/adminApp/views.py
@@ -0,0 +1,162 @@
+from django.shortcuts import render
+from django.http import *
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate,login, logout
+from .models import *
+from django.core.exceptions import ObjectDoesNotExist
+import csv
+
+
+def loginForm(request):
+    if request.user.is_authenticated():
+        return HttpResponseRedirect("welcome")
+    username = password = ''
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect("welcome")
+
+        if username == 'admin1' and password == 'admin1':
+            return HttpResponseRedirect("welcome")
+
+    return render(request, 'adminApp/loginForm.html')
+
+
+@login_required(login_url='/adminApp/')
+def welcome(request):
+    with open("adminApp/students.csv", 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            myrollno = row[0]
+            myname = row[1]
+            mypassword = myrollno
+            myemail = row[3]
+            if (not student.objects.filter(roll_number=myrollno).exists()) and (not User.objects.filter(username=myname).exists()):
+                newUser = User(username=myname, password=mypassword, email=myemail)
+                newUser.save()
+                newStudent = student(roll_number=myrollno, user=newUser)
+                newStudent.save()
+    all_courses = course.objects.all()
+    context = {'all_courses': all_courses}
+    return render(request, 'adminApp/welcome.html', context)
+
+
+def addCourse(request):
+    if request.POST:
+        coursename = request.POST['courseName']
+        coursenumber = request.POST['courseNumber']
+        if coursename=="" or coursenumber=="":
+            return HttpResponseRedirect("/adminApp/welcome")
+        newCourse = course(course_name = coursename,course_number = coursenumber)
+        newCourse.save()
+    return HttpResponseRedirect("/adminApp/welcome")
+
+
+def coursePage(request,course_number):
+    all_students = student.objects.all()
+    context = {"course_number": course_number, "all_students": all_students}
+    return render(request,"adminApp/coursePage.html", context)
+
+
+def enroll(request):
+    if request.POST:
+        selectedStudents = request.POST.getlist('selected')
+        coursenumber = request.POST['course_num']
+        currCourse = course.objects.get(course_number=coursenumber)
+        for curr_pk in selectedStudents:
+            student.objects.get(pk=curr_pk).courses.add(currCourse)
+    return HttpResponseRedirect("/adminApp/welcome")
+
+
+def logout_admin(request):
+    logout(request)
+    return HttpResponseRedirect('/adminApp/')
+
+
+def deadlineform(request,course_number):
+    thiscourse = course.objects.get(course_number=course_number)
+    context ={"course": thiscourse}
+    return render(request,"adminApp/deadline.html", context)
+
+
+def mid_deadline(request):
+    if request.POST:
+        coursenumber = request.POST['course_num']
+        currcourse = course.objects.get(course_number=coursenumber)
+        formdate = request.POST['midDate']
+        currcourse.mid_deadline = formdate
+        currcourse.save()
+    return HttpResponseRedirect("/adminApp/deadlineform/"+coursenumber)
+
+
+def end_deadline(request):
+    if request.POST:
+        coursenumber = request.POST['course_num']
+        currcourse = course.objects.get(course_number=coursenumber)
+        formdate = request.POST['endDate']
+        currcourse.end_deadline = formdate
+        currcourse.save()
+    return HttpResponseRedirect("/adminApp/deadlineform/"+coursenumber)
+
+
+def feedbackform(request,course_number):
+    coursecurrent = course.objects.get(course_number=course_number)
+    context ={"course": coursecurrent, 'endques': coursecurrent.endsemquestion_set.all(), 'midques': coursecurrent.midsemquestion_set.all()}
+    return render(request,"adminApp/feedback.html",context)
+
+
+def mid_feedback(request):
+    if request.POST:
+        coursenumber = request.POST['course_num']
+        currcourse = course.objects.get(course_number=coursenumber)
+        allques = currcourse.midsemquestion_set.all()
+        q1 = request.POST['q1']
+        q2 = request.POST['q2']
+        if allques.count() == 0:
+            myques = midsemquestion(ques=q1)
+            myques.course = currcourse
+            myques.save()
+            myques = midsemquestion(ques=q2)
+            myques.course = currcourse
+            myques.save()
+            currcourse.save()
+        else:
+            myq = midsemquestion.objects.get(ques=allques[0])
+            myq.ques = q1
+            myq.save()
+            myq = midsemquestion.objects.get(ques=allques[1])
+            myq.ques = q2
+            myq.save()
+    return HttpResponseRedirect("/adminApp/feedbackform/"+coursenumber)
+
+
+def end_feedback(request):
+    if request.POST:
+        coursenumber = request.POST['course_num']
+        currcourse = course.objects.get(course_number=coursenumber)
+        allques = currcourse.endsemquestion_set.all()
+        q1 = request.POST['q1']
+        q2 = request.POST['q2']
+        if allques.count() == 0:
+            myques = endsemquestion(ques=q1)
+            myques.course = currcourse
+            myques.save()
+            myques = endsemquestion(ques=q2)
+            myques.course = currcourse
+            myques.save()
+            currcourse.save()
+        else:
+            myq = endsemquestion.objects.get(ques=allques[0])
+            myq.ques = q1
+            myq.save()
+            myq = endsemquestion.objects.get(ques=allques[1])
+            myq.ques = q2
+            myq.save()
+
+    return HttpResponseRedirect("/adminApp/feedbackform/"+coursenumber)
+
