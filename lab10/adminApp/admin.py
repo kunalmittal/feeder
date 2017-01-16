32c56254212b4155a1bf81b8fee7cafc656499f9
KUNAL MITTAL <kunal@cse.iitb.ac.in>
2017-01-16 22:27:06 +0530

    Pushing to github

--- lab10/adminApp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from adminApp.models import *
-
-admin.site.register(course)
-admin.site.register(student)
-admin.site.register(midsemquestion)
-admin.site.register(endsemquestion)
-
